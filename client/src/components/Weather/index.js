b345eb804f7f1348068be5b656325d8f0856148a
Jigao Zeng <zengjigao@gmail.com>
2021-03-17 12:36:50 +1300

    convert class component to functional component
    convert class component to functional component

--- client/src/components/Weather/index.js
+++ client/src/components/Weather/index.js
@@ -1,108 +1,87 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import ReactDOM from 'react-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-
 import { faCloud } from '@fortawesome/free-solid-svg-icons';
 import { faCloudRain } from '@fortawesome/free-solid-svg-icons';
 import { faBolt } from '@fortawesome/free-solid-svg-icons';
 import { faSnowflake } from '@fortawesome/free-solid-svg-icons';
 import { faSun } from '@fortawesome/free-solid-svg-icons';
 import { faSmog } from '@fortawesome/free-solid-svg-icons';
-import './weatherStyle.css';
-
-
-
+import './style.css';
 
 const API_KEY = "8e20c3b423d1a8139959af74dcb9cba2";
-//const BASE = "https://api.openweathermap.org/data/2.5/";
+const weatherIcon = {
+    Thunderstorm: <FontAwesomeIcon icon={faBolt} />,
+    Rain: <FontAwesomeIcon icon = {faCloudRain} />,
+    Snow:  <FontAwesomeIcon icon={faSnowflake} />,
+    Atmosphere: <FontAwesomeIcon icon={faSmog} />,
+    Clear: <FontAwesomeIcon icon={faSun} />,
+    Clouds: <FontAwesomeIcon icon={faCloud} />,
+}
 
-
-class Weather extends React.Component {
-    constructor() {
-      super();
-      this.state = {
-        icon: undefined,
-        main: undefined,
-        celsius: undefined,
-        temp_max: null,
-        temp_min: null,
-        description: "",
-        error: false
-      };
-  
-      this.weatherIcon = {
-        Thunderstorm: <FontAwesomeIcon icon={faBolt} />,
-        //Drizzle: "wi-sleet",
-        Rain: <FontAwesomeIcon icon = {faCloudRain} />,
-        Snow:  <FontAwesomeIcon icon={faSnowflake} />,
-        Atmosphere: <FontAwesomeIcon icon={faSmog} />,
-        Clear: <FontAwesomeIcon icon={faSun} />,
-        Clouds: <FontAwesomeIcon icon={faCloud} />,
-      };
-    }
+function Weather()  {
     
+
+    const [icon,setIcon] = useState(undefined);
+    const [main,setMain] = useState(undefined);
+    const [celsius, setCelsius] = useState(undefined);
+    const [temp_max, setMaxTemp] = useState(null);
+    const [temp_min, setMinTemp] = useState(null);
+    const [description, setDescription] = useState("");
+   
     /* the get_WeatherIcon functions set the weather icon based on the weather condition codes
        see https://openweathermap.org/weather-conditions for more details
+       cloud drizzle icon needs pro plan, so the cloud rain icon is used instead
     */
-    get_WeatherIcon(icons, rangeId) {
+    const get_WeatherIcon = (rangeId) => {
       switch (true) {
         case rangeId >= 200 && rangeId < 232:
-          this.setState({ icon: icons.Thunderstorm });
+          setIcon(weatherIcon.Thunderstorm);
           break;
         case rangeId >= 300 && rangeId <= 321:
-          this.setState({ icon: icons.Rain });
+          setIcon(weatherIcon.Rain);
           break;
         case rangeId >= 500 && rangeId <= 521:
-          this.setState({ icon: icons.Rain });
+          setIcon(weatherIcon.Rain);
           break;
         case rangeId >= 600 && rangeId <= 622:
-          this.setState({ icon: icons.Snow });
+          setIcon(weatherIcon.Snow);
           break;
         case rangeId >= 701 && rangeId <= 781:
-          this.setState({ icon: icons.Atmosphere });
+          setIcon(weatherIcon.Atmosphere);
           break;
         case rangeId === 800:
-          this.setState({ icon: icons.Clear });
+          setIcon(weatherIcon.Clear);
           break;
         case rangeId >= 801 && rangeId <= 804:
-          this.setState({ icon: icons.Clouds });
+          setIcon(weatherIcon.Clouds);
           break;
         default:
-          this.setState({ icon: icons.Clouds });
+          setIcon(weatherIcon.Clouds);
       }
     }
+    
   
-    toCelsius(temp) {
+    const toCelsius = (temp) => {
       let cell = Math.floor(temp - 273.15);
       return cell;
     }
 
-  
-  
-    componentDidMount() {
-        let response = null;
+   useEffect(() =>{
         fetch( `http://api.openweathermap.org/data/2.5/weather?q=Auckland&appid=${API_KEY}` )
             .then(res => res.json())
             .then(response => {
-                this.setState({
-          
-                    main: response.weather[0].main,
-                    celsius: this.toCelsius(response.main.temp),
-                    temp_max: this.toCelsius(response.main.temp_max),
-                    temp_min: this.tpCelsius(response.main.temp_min),
-                    description: response.weather[0].description,
-                    error: false
+                    setMain(response.weather[0].main);
+                    setCelsius(toCelsius(response.main.temp));
+                    setMaxTemp(toCelsius(response.main.temp_max));
+                    setMinTemp(toCelsius(response.main.temp_min));
+                    setDescription(response.weather[0].description);
+                    get_WeatherIcon(response.weather[0].id);
                   });
-                this.get_WeatherIcon(this.weatherIcon, response.weather[0].id);
-        });
-  
-       
-    } 
+
+    },[]);
     
-  
-    render() {
-     
-      return (
+    return (
         <div className="container">
             
                 <div className="weather-text-box">
@@ -112,30 +91,21 @@ class Weather extends React.Component {
                 <div className="current-weather-box">
             
                     <div className="icon-container">
-                        
-                        {this.state.icon}
+                        {icon}
                     </div>
 
                     <div className="discription">
-                        <div className="weather-NZ">{this.state.description}|Auckland, NZ</div>
-                        <div className="temp">{this.state.celsius}°C</div>
-                        <div className = "temp-bounds">L:{this.state.temp_min}°C|H:{this.state.temp_max}°C</div>
+                        <div className="weather-NZ">{description}|Auckland, NZ</div>
+                        <div className="temp">{celsius}°C</div>
+                        <div className = "temp-bounds">L:{temp_min}°C|H:{temp_max}°C</div>
                     </div>
                    
                 </div>
         </div>
         
-
-    
      )
-    }
+    
   }
-
-
-
-
-
-
 
 
 export default Weather;
