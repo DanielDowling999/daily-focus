1a297808cd4277f1e2225cdf475782073612ca78
DanielDowling999 <62310275+DanielDowling999@users.noreply.github.com>
2021-04-17 19:47:27 +1200

    added some tests, still need to figure out how to test api bits

--- client/src/components/Weather/Weather.test.js
+++ client/src/components/Weather/Weather.test.js
@@ -1,10 +1,16 @@
 import React from "react";
 import { shallow } from "enzyme";
 import ShallowRenderer from "react-test-renderer/shallow";
+import styles from "./style.module.scss";
 
 import Weather from "./";
-
+import dailyData from "./";
+//const getTodaysDate = require("../utils/date-helper");
+//let todaysDate = getTodaysDate();
+//import getWeatherData from "./";
 let component;
+const lat = -36.85;
+const lon = 174.76;
 
 beforeEach(() => {
     component = shallow(<Weather />);
@@ -14,4 +20,13 @@ test("it renders correctly", () => {
     const shallowRenderer = new ShallowRenderer();
     const snapshotComponent = shallowRenderer.render(<Weather />);
     expect(snapshotComponent).toMatchSnapshot();
+});
+//All of these cases are almost definitely covered by the snapshot test.
+test("it contains DayCards and HourCards", () => {
+    expect(component.find(".hourlyForecast")).toHaveLength(1);
+    expect(component.find(".dailyForecast")).toHaveLength(1);
+});
+
+test("it contains an icon for main", () => {
+    expect(component.find(".iconContainer")).toHaveLength(1);
 });
